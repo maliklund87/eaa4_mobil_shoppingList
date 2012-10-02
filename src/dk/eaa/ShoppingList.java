82ccda3b16a903bbfb0391de8d30e0b3deaf01f5
Malik Lund <maliklund87@gmail.com>
2012-10-02 14:08:43 +0200

    updated ignore list 2

---
+++ src/dk/eaa/ShoppingList.java
@@ -0,0 +1,15 @@
+package dk.eaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingList extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
