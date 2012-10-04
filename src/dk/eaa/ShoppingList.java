8a1e24e71c89324d1a166aea39a56f431d791987
Lars Nielsen <lnc13.lars@gmail.com>
2012-10-04 20:00:37 +0200

    Lars: Begging the shoplist fragment and the code behinde

--- src/dk/eaa/ShoppingList.java
+++ src/dk/eaa/ShoppingList.java
@@ -2,6 +2,9 @@ package dk.eaa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ShoppingList extends Activity {
     /**
@@ -12,4 +15,17 @@ public class ShoppingList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        return inflater.inflate(R.layout.shoplistfragment, container, false);
+
+    }
+
+    public void OnPause(){}
+
+
+
+
+
 }
