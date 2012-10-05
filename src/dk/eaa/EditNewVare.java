3f6287b03a0e8014983d725e155a12109e8674a4
Martin R. Bundgaard <martin@ronn-bundgaard.dk>
2012-10-05 09:15:43 +0200

    Added editNewVare + actionbar to same

---
+++ src/dk/eaa/EditNewVare.java
@@ -0,0 +1,11 @@
+package dk.eaa;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Martin R. Bundgaard
+ * Date: 05-10-12
+ * Time: 08:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class EditNewVare {
+}
