8338b2cdfaddb90793797a3f45c5ca40335256d4
Martin R. Bundgaard <martin@ronn-bundgaard.dk>
2012-10-05 09:17:00 +0200

    Added editNewMenu

--- src/dk/eaa/EditNewVare.java
+++ src/dk/eaa/EditNewVare.java
@@ -1,5 +1,10 @@
 package dk.eaa;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Martin R. Bundgaard
@@ -7,5 +12,17 @@ package dk.eaa;
  * Time: 08:57
  * To change this template use File | Settings | File Templates.
  */
-public class EditNewVare {
+public class EditNewVare extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.new_edit_menu, menu);
+        return true;
+
+    }
 }
