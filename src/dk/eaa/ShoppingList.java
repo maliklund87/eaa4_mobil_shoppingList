95642bba52409e0d8fc6d43d24a5530a64d2f0fa
Lars Nielsen <lnc13.lars@gmail.com>
2012-10-07 12:33:39 +0200

    This is just a test commit, do to the fact I can't merge

--- src/dk/eaa/ShoppingList.java
+++ src/dk/eaa/ShoppingList.java
@@ -2,10 +2,14 @@ package dk.eaa;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.*;
 
 public class ShoppingList extends Activity {
+
+    Fragment framgent1, fragment2;
+
     /**
      * Called when the activity is first created.
      */
@@ -33,5 +37,19 @@ public class ShoppingList extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    public static class FirstMenuFragment extends Fragment{
+
+        @Override
+        public void onCreate(Bundle saveInstanceState){
+            super.onCreate(saveInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+
+        }
     }
 }
