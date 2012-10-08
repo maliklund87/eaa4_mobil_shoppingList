1dc5a44015cf8566f92f89adfb420b42246ab13d
Martin R. Bundgaard <martin@ronn-bundgaard.dk>
2012-10-08 21:37:50 +0200

    Tilføjet gem funktioner til editNew

--- src/dk/eaa/WaresFragment.java
+++ src/dk/eaa/WaresFragment.java
@@ -67,11 +67,11 @@ public class WaresFragment extends Fragment {
         Cursor cur = db.getAllWares();
 
         while(cur.moveToNext()){
-            int waresId = cur.getInt(1);
-            String waresName = cur.getString(2);
-            double waresPrice = cur.getDouble(3);
-            String wareUnit = cur.getString(4);
-            double waresAmount = cur.getDouble(5);
+            int waresId = cur.getInt(0);
+            String waresName = cur.getString(1);
+            double waresPrice = cur.getDouble(2);
+            String wareUnit = cur.getString(3);
+            double waresAmount = cur.getDouble(4);
 
             Ware ware = new Ware(waresName);
             ware.setPrice(waresPrice);
